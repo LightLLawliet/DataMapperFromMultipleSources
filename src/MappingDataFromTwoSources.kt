86eef473fa96dfe2780e283720d00d2ba4603df1
NumpyGlumpy <khaypov03@mail.ru>
2023-02-15 23:03:39 +0300

    - 002

--- src/MappingDataFromTwoSources.kt
+++ src/MappingDataFromTwoSources.kt
@@ -20,16 +20,27 @@ interface B {
     }
 
     class Base(private val number: Int) : B {
-        override fun <T> map(mapper: Mapper<T>): T {
-            return mapper.map(number)
-        }
+        override fun <T> map(mapper: Mapper<T>): T = mapper.map(number)
     }
 }
 
 data class C(private val data: String, private val number: Int)
 
 class ComplexMapper(private val b: B) : A.Mapper<C> {
-    override fun map(data: String): C = b.map(object : B.Mapper<C> {
-        override fun map(number: Int): C = C(data, number)
-    })
+    override fun map(data: String): C {
+        val mapper = object : B.Mapper<C> {
+            override fun map(number: Int): C = C(data.ifEmpty { throw InvalidDataException() }, number)
+        }
+        return b.map(mapper)
+    }
+}
+
+class InvalidDataException(override val message: String = "invalid data") :
+    java.lang.IllegalStateException(message)
+
+fun main() {
+    val a = A.Base("a")
+    val b = B.Base(1)
+    val c = a.map(ComplexMapper(b))
+    println(c)
 }
